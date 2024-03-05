f669e2d6887dfea655dcafa56efe27b953900399
Ronney Nigro <38004166+rbnigro@users.noreply.github.com>
2024-03-05 12:58:34 -0300

    Other Commit

--- src/main/java/com/messageria/config/KafkaConsumerConfig.java
+++ src/main/java/com/messageria/config/KafkaConsumerConfig.java
@@ -28,22 +28,19 @@ public class KafkaConsumerConfig {
 	ProducerFactory<String, String> producerFactory(){
 		Map<String, Object> properties = kafkaProperties.buildProducerProperties();
 		return new DefaultKafkaProducerFactory<>(properties);
-		// Retorna um objeto
 	}
 	
 	@Bean
 	KafkaTemplate<String, String> kafkaTemplate() {
 		return new KafkaTemplate<>(producerFactory());
-		// Retonra um objeto
 	}
 	
 	@Bean
 	NewTopic pagamentoRequestTopicBuilder() {
 		return TopicBuilder
 				.name("pagamento.request.topic.v1")
-			//	.partitions(1)
-			//	.replicas(1)
+				.partitions(1)
+				.replicas(1)
 				.build();
-		// Retonra um objeto
 	}
 }
