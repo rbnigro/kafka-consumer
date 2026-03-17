176284284a5455a06526cb268448435a1a38882e
Ronney Nigro <ronneynigro@gmail.com>
2026-03-17 17:02:11 -0300

    Integração três projetos

--- src/main/java/com/messageria/consumer/PagamentoRequestConsumer.java
+++ src/main/java/com/messageria/consumer/PagamentoRequestConsumer.java
@@ -1,4 +1,4 @@
-package com.messageria.consumer;
+package main.java.com.messageria.consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 public class PagamentoRequestConsumer {
 	
 	
-	@KafkaListener(topics = "${spring.kafka.producer.pagamento.request.topic.v1}", groupId = "pagamento-request-consumer1")
+	@KafkaListener(topics = "${app.topic.pagamento}", groupId = "pagamento-request-consumer1")
 	public void consumer(String message) {
 		System.out.println("==== MENSAGEM RECEBIDA ==== " + message);
 	}
