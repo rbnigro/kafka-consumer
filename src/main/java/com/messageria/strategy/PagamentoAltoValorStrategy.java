8bc0e4b37716041838af8ec1eda15fc422812058
Ronney Nigro <ronneynigro@gmail.com>
2026-03-19 07:24:36 -0300

    Correções Regra Negocio

--- src/main/java/com/messageria/strategy/PagamentoAltoValorStrategy.java
+++ src/main/java/com/messageria/strategy/PagamentoAltoValorStrategy.java
@@ -1,13 +1,20 @@
-package com.messageria.service;
+package com.messageria.strategy;
 
 import com.messageria.dto.PagamentoDTO;
-import com.messageria.strategy.PagamentoStrategy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
+@Component
 public class PagamentoAltoValorStrategy  implements PagamentoStrategy {
+
+    @Value("${pagamento.limite-alto-valor}")
+    private BigDecimal limite;
 
     @Override
     public boolean seAplica(PagamentoDTO dto) {
-        return dto.getValor().doubleValue() > 1000;
+        return dto.getValor().compareTo(limite) > 0;
     }
 
     @Override
---
+++ src/main/java/com/messageria/strategy/PagamentoNormalStrategy.java
@@ -0,0 +1,25 @@
+package com.messageria.strategy;
+
+import com.messageria.dto.PagamentoDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class PagamentoNormalStrategy  implements PagamentoStrategy {
+
+    @Value("${pagamento.limite-alto-valor}")
+    private BigDecimal limite;
+
+    @Override
+    public boolean seAplica(PagamentoDTO dto) {
+        return dto.getValor().compareTo(limite) <= 0;
+    }
+
+    @Override
+    public void processar(PagamentoDTO dto) {
+        dto.setDescricao(dto.getDescricao() + " [NORMAL]");
+    }
+
+}
